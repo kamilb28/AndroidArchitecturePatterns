d4b0c93eef9fa038ba89fc92f570dc05b133d97c
Kamil Bernacik <kbernacik@core-logic.com>
2024-11-10 21:42:23 +0100

    Opt

--- app/src/main/java/com/example/patterns/screens/WelcomeScreen.kt
+++ app/src/main/java/com/example/patterns/screens/WelcomeScreen.kt
@@ -1,9 +1,16 @@
 package com.example.patterns.screens
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
